b910d6fd9e82ebd25533c4447c428b02616b1aaf
Amonizz <andrea.monizza@gmail.com>
2025-05-06 13:34:26 +0200

    adding crud + tests

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
@@ -1,9 +1,25 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import org.springframework.data.domain.Page;
 
+import java.util.UUID;
+
 public interface BeerClient {
+    BeerDTO getBeerById(UUID beerId);
+
+    BeerDTO createBeer(BeerDTO beerDTO);
+
+    void deleteBeer(UUID beerId);
+
+    BeerDTO updateBeer(BeerDTO beerDTO);
 
     Page<BeerDTO> listBeers();
+
+    Page<BeerDTO> listBeers(String beerName,
+                            BeerStyle beerStyle,
+                            Boolean showInventory,
+                            Integer pageNumber,
+                            Integer pageSize);
 }
--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,8 +1,13 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,9 +17,138 @@ class BeerClientImplTest {
     @Autowired
     BeerClientImpl beerClient;
 
-    @Test
-    void listBeer() {
 
-        beerClient.listBeers();
+    @Test
+    void deleteBeer() {
+        BeerDTO newDTO = BeerDTO.builder()
+                .price(new BigDecimal("10.99"))
+                .beerName("Mango Monny")
+                .beerStyle(BeerStyle.GOSE)
+                .quantityOnHand(500)
+                .upc("123245")
+                .build();
+
+        BeerDTO saved = this.beerClient.createBeer(newDTO);
+        System.out.println(saved);
+        assertThrows(HttpClientErrorException.class, () -> this.beerClient.deleteBeer(newDTO.getId()));
     }
+
+    @Test
+    void updateBeer() {
+
+        BeerDTO savedBeer = beerClient.listBeers().getContent().get(0);
+
+        final String newName = "Mango Fede 2";
+        System.out.println(savedBeer);
+
+        savedBeer.setBeerName(newName);
+        BeerDTO updatedBeer = beerClient.updateBeer(savedBeer);
+
+        System.out.println(updatedBeer);
+
+        assertEquals(newName, updatedBeer.getBeerName());
+    }
+
+    @Test
+    void getBeerById() {
+
+        BeerDTO savedBeer = this.beerClient.listBeers().getContent().get(0);
+        BeerDTO beer = beerClient.getBeerById(savedBeer.getId());
+
+        assertNotNull(beer);
+    }
+
+    @Test
+    void listBeerNoName() {
+
+        beerClient.listBeers(null,
+                null,
+                null,
+                null,
+                null
+                );
+    }
+
+    @Test
+    void listBeerWBeerName() {
+
+        beerClient.listBeers("ALE",
+                null,
+                null,
+                null,
+                null
+                );
+    }
+
+    @Test
+    void listBeerWBeerStyle() {
+
+        beerClient.listBeers(null,
+//                EnumUtils.findEnumInsensitiveCase(BeerStyle.class, "PALE_ALE"),
+                BeerStyle.PALE_ALE,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Test
+    void listBeerWShowInvTrue() {
+
+        beerClient.listBeers(null,
+                null,
+                true,
+                null,
+                null
+        );
+    }
+
+    @Test
+    void listBeerWShowInvFalse() {
+
+        beerClient.listBeers(null,
+                null,
+                false,
+                null,
+                null
+        );
+    }
+
+    @Test
+    void listBeerWPageNumb() {
+
+        beerClient.listBeers(null,
+                null,
+                null,
+                3,
+                null
+        );
+    }
+
+    @Test
+    void listBeerWPageSize() {
+
+        beerClient.listBeers(null,
+                null,
+                null,
+                null,
+                35
+        );
+    }
+
+    @Test
+    void createNewBeer() {
+        BeerDTO newDTO = BeerDTO.builder()
+                .price(new BigDecimal("10.99"))
+                .beerName("Mango Monny")
+                .beerStyle(BeerStyle.GOSE)
+                .quantityOnHand(500)
+                .upc("123245")
+                .build();
+
+        BeerDTO saved = this.beerClient.createBeer(newDTO);
+        System.out.println(saved);
+        assertNotNull(saved);
+    }
+
 }
